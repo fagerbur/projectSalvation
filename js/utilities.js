12379cf344cb5d2d3c9bf7ba7c2daded296c721b
jfagerb <jfagerb@gmail.com>
2021-07-14 21:00:01 -0500

    and the device configuration begins..

--- js/utilities.js
+++ js/utilities.js
@@ -2,18 +2,19 @@ AFRAME.registerComponent('title-scroll', {
     init:function() {
       this.el.addEventListener('click', function (evt) {
         var title = document.getElementById("WelcomeText")
-        title.setAttribute("animation", "property: position; to: .45 3 -2; dur: 3000; easing: linear; delay: 1000; autoplay: true;")
+        title.setAttribute("animation", "property: position; to:0 3.025 -2; dur: 3000; easing: linear; delay: 1000; autoplay: true;")
 
         var introduction = document.getElementById("IntroductionText")
         introduction.setAttribute("animation","property: text.opacity; to: 1; dur: 3000; easing: linear; delay:3500; autoplay: true;")
         
         var titlepanel = document.getElementById("TitlePanel")
-        titlepanel.setAttribute("animation","property: height; to:2.25; dur:3000; easing:linear; delay:1000; autoplay:true;")
-        titlepanel.setAttribute("animation__2","property: position; to:0 2.1 -2.025; dur:3000; easing:linear; delay:1000; autoplay:true;")
+        titlepanel.setAttribute("animation","property: height; to:2.5; dur:3000; easing:linear; delay:1000; autoplay:true;")
+        titlepanel.setAttribute("animation__2","property: position; to:0 2 -2.025; dur:3000; easing:linear; delay:1000; autoplay:true;")
 
         document.getElementById("Start").setAttribute("animation","property: visible; to:false; delay:1000; autoplay:true;")
         
         document.getElementById("NextOne").setAttribute("animation","property: visible; to:true; dur:3500; easing:linear; autoplay:true;")
+        document.getElementById("Music").components.sound.playSound();
       });
     }
 });
@@ -24,9 +25,7 @@ AFRAME.registerComponent('next-one', {
         var camera = document.getElementById("CameraRig")
         var x = camera.getAttribute("position").x + 10
         camera.setAttribute("animation","property: position; to: "+ x +" 1.6 0; dur: 5000; easing: easeInOutQuad; delay:1000; autoplay: true;")
-
-        // document.getElementById("Beginning").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
-        // document.getElementById("PreExistence").setAttribute("animation","property: visible; to:true; delay:1000; autoplay:true;")
+        updatePositionVisibility(x);
     });
   }
 });
@@ -37,18 +36,73 @@ AFRAME.registerComponent('prev-one', {
           var camera = document.getElementById("CameraRig")
           var x = camera.getAttribute("position").x - 10
           camera.setAttribute("animation","property: position; to: "+ x +" 1.6 0; dur: 5000; easing: easeInOutQuad; delay:1000; autoplay: true;")
-  
-        //   document.getElementById("Beginning").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
-        //   document.getElementById("PreExistence").setAttribute("animation","property: visible; to:true; delay:1000; autoplay:true;")
+          updatePositionVisibility(x);
       });
     }
   });
+
+function updatePositionVisibility(position)
+{
+    if(position === 0)
+    {
+        document.getElementById("Beginning").setAttribute("animation","property: visible; to:true; delay:1000; autoplay:true;")
+        document.getElementById("PreExistence").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+    }
+    else if(position === 10)
+    {
+        document.getElementById("Beginning").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+        document.getElementById("PreExistence").setAttribute("animation","property: visible; to:true; delay:1000; autoplay:true;")
+        document.getElementById("CreationFall").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+    }
+    else if(position === 20)
+    {
+        document.getElementById("PreExistence").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+        document.getElementById("CreationFall").setAttribute("animation","property: visible; to:true; delay:1000; autoplay:true;")
+        document.getElementById("EarthLife").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+    }
+    else if(position === 30)
+    {
+        document.getElementById("CreationFall").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+        document.getElementById("EarthLife").setAttribute("animation","property: visible; to:true; delay:1000; autoplay:true;")
+        document.getElementById("Atonement").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+    }
+    else if(position === 40)
+    {
+        document.getElementById("EarthLife").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+        document.getElementById("Atonement").setAttribute("animation","property: visible; to:true; delay:1000; autoplay:true;")
+        document.getElementById("SpiritWorld").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+    }
+    else if(position === 50)
+    {
+        document.getElementById("Atonement").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+        document.getElementById("SpiritWorld").setAttribute("animation","property: visible; to:true; delay:1000; autoplay:true;")
+        document.getElementById("Resurrection").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+    }
+    else if(position === 60)
+    {
+        document.getElementById("SpiritWorld").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+        document.getElementById("Resurrection").setAttribute("animation","property: visible; to:true; delay:1000; autoplay:true;")
+        document.getElementById("Kingdoms").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+    }
+    else if(position === 70)
+    {
+        document.getElementById("Resurrection").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+        document.getElementById("Kingdoms").setAttribute("animation","property: visible; to:true; delay:1000; autoplay:true;")
+        document.getElementById("Conclusion").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+    }
+    else if(position === 80)
+    {
+        document.getElementById("Kingdoms").setAttribute("animation","property: visible; to:false; delay:4000; autoplay:true;")
+        document.getElementById("Conclusion").setAttribute("animation","property: visible; to:true; delay:1000; autoplay:true;")
+    }
+};
 
 AFRAME.registerComponent("play-control", {
     init: function () {
         this.el.addEventListener("click", function(e) { 
             var christVideo = document.getElementById("BecauseOfHim");
             var videoControl = document.getElementById("PauseControl");
+            document.getElementById("Music").components.sound.pauseSound();
 
             christVideo.play();
             videoControl.setAttribute("scale",".25 .25");
@@ -62,9 +116,21 @@ AFRAME.registerComponent("pause-control", {
         this.el.addEventListener("click", function(e) { 
             var christVideo = document.getElementById("BecauseOfHim");
             var videoControl = document.getElementById("PlayControl");
+            document.getElementById("Music").components.sound.playSound();
+
             christVideo.pause();
             videoControl.setAttribute("scale",".5 .5");
             e.target.setAttribute("scale","0 0");
+        });
+    }
+});
+
+AFRAME.registerComponent("video-end", {
+    init: function() {
+        document.getElementById("BecauseOfHim").addEventListener("ended", (e) => {
+            document.getElementById("Music").components.sound.playSound();
+            document.getElementById("PlayControl").setAttribute("scale",".5 .5");
+            document.getElementById("PauseControl").setAttribute("scale","0 0");
         });
     }
 });
