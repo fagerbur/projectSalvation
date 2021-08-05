b837b22f00d215e06444966069e9d0d575b73066
jfagerb <jfagerb@gmail.com>
2021-08-04 21:16:59 -0500

    almost there...

--- js/device_config.js
+++ js/device_config.js
@@ -1,4 +1,4 @@
-document.querySelector('a-scene').addEventListener('enter-vr', function () {
+function isMobile() {
     var hasTouchScreen = false;
     if ("maxTouchPoints" in navigator) {
         hasTouchScreen = navigator.maxTouchPoints > 0;
@@ -19,41 +19,35 @@ document.querySelector('a-scene').addEventListener('enter-vr', function () {
             );
         }
     }
-    if (hasTouchScreen)
+
+    return hasTouchScreen;
+}
+
+if(navigator.userAgent.includes("Mozilla"))
+{
+    var mobileCheck = isMobile();
+    if(mobileCheck)
     {
-        if(window.navigator.userAgent.includes("Mozilla"))
-        {
-            document.getElementById('Scene').setAttribute("vr-mode-ui","enabled: false");
-        }
+        document.getElementById('Scene').setAttribute("vr-mode-ui","enabled: false");
+    }
+}
+
+document.querySelector('a-scene').addEventListener('enter-vr', function () {
+    var mobileCheck = isMobile();
+
+    if (mobileCheck)
+    {
         var gazeCursor = document.createElement('a-cursor');
         document.getElementById('Camera').appendChild(gazeCursor);
     }
-  });
+});
 
-  document.querySelector('a-scene').addEventListener('exit-vr', function () {
-    var hasTouchScreen = false;
-    if ("maxTouchPoints" in navigator) {
-        hasTouchScreen = navigator.maxTouchPoints > 0;
-    } else if ("msMaxTouchPoints" in navigator) {
-        hasTouchScreen = navigator.msMaxTouchPoints > 0;
-    } else {
-        var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
-        if (mQ && mQ.media === "(pointer:coarse)") {
-            hasTouchScreen = !!mQ.matches;
-        } else if ('orientation' in window) {
-            hasTouchScreen = true; // deprecated, but good fallback
-        } else {
-            // Only as a last resort, fall back to user agent sniffing
-            var UA = navigator.userAgent;
-            hasTouchScreen = (
-                /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
-                /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
-            );
-        }
-    }
-    if (hasTouchScreen)
+document.querySelector('a-scene').addEventListener('exit-vr', function () {
+    var mobileCheck = isMobile();
+
+    if (mobileCheck)
     {
         var gazeCursor = document.querySelector('a-cursor')
         document.getElementById('Camera').removeChild(gazeCursor);
     }
-  });
+});
